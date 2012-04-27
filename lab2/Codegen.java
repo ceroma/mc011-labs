54e7469eedfdb284616a5eda7117ba0370950774
Carlos Eduardo Rosa Machado <carlos.rmachado@ymail.com>
2012-04-26 21:01:38 -0700

    Initializing lab2 folder.

---
+++ lab2/Codegen.java
@@ -0,0 +1,15 @@
+package x86;
+
+import util.List;
+
+import tree.Stm;
+import assem.Instr;
+
+public class Codegen{
+    public Codegen(Frame f){
+    }
+
+    public List<Instr> codegen(List<Stm> body){
+        return null;
+    }
+}
