116008ed35da023ef4965e7ede82dfea846c651a
Carlos Eduardo Rosa Machado <carlos.rmachado@ymail.com>
2012-04-28 18:18:52 -0700

    Book's initial template for Codegen.

--- lab2/Codegen.java
+++ lab2/Codegen.java
@@ -2,14 +2,44 @@ package x86;
 
 import util.List;
 
+import tree.Exp;
 import tree.Stm;
+import temp.Temp;
 import assem.Instr;
 
-public class Codegen{
-    public Codegen(Frame f){
+public class Codegen {
+	Frame frame;
+	private List<Instr> ilist = null, last = null;
+	
+    public Codegen(Frame f) {
+    	frame = f;
+    }
+    
+    private void emit(Instr inst) {
+    	if (last != null) {
+    		last = last.tail = new List<Instr>(inst, null);
+    	} else {
+    		last = ilist = new List<Instr>(inst, null);
+    	}
     }
 
-    public List<Instr> codegen(List<Stm> body){
+    void munchStm(Stm s) {
+    	return;
+    }
+    
+    Temp munchExp(Exp e) {
+    	return new Temp();
+    }
+    
+    public List<Instr> codegen(Stm s) {
+    	List<Instr> l;
+    	munchStm(s);
+    	l = ilist;
+    	ilist = last = null;
+    	return l;
+    }
+
+    public List<Instr> codegen(List<Stm> body) {
         return null;
     }
 }
