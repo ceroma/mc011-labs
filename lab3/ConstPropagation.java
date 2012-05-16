edaa6f69b4e70f476ac0d7f7232bbc9c4f13c92e
Carlos Eduardo Rosa Machado <carlos.rmachado@ymail.com>
2012-05-16 17:49:49 -0300

    Fix indentation.

--- lab3/ConstPropagation.java
+++ lab3/ConstPropagation.java
@@ -10,31 +10,29 @@ import graph.Node;
 
 public class ConstPropagation {
 
-	private ReachingDefinition dfa;
-	private AssemFlowGraph cfg;
+    private ReachingDefinition dfa;
+    private AssemFlowGraph cfg;
 
-	/*
-	 * Realiza a otimização Constant Propagation
-	 */
-	public void optimize(List<Instr> l) {
-		cfg = new AssemFlowGraph(l);
-		dfa = new ReachingDefinition(l, cfg);
+    /**
+     * Realiza a otimização Constant Propagation
+     */
+    public void optimize(List<Instr> l) {
+        cfg = new AssemFlowGraph(l);
+        dfa = new ReachingDefinition(l, cfg);
 
-                /*
-                    IMPLEMENTAR AQUI SEU CÓDIGO
-                 */
-	}
+        /* IMPLEMENTAR AQUI SEU CÓDIGO */
+    }
 
-	private void propagateConstant(Instr instr, String cte) {
-		String assembly = instr.getAssembly();
-		int index = assembly.indexOf(",");
-		String newAssembly = assembly.substring(0, index + 2) + cte;
-		instr.setAssembly(newAssembly.replaceAll("mov", "mov dword"));
-	}
+    private void propagateConstant(Instr instr, String cte) {
+        String assembly = instr.getAssembly();
+        int index = assembly.indexOf(",");
+        String newAssembly = assembly.substring(0, index + 2) + cte;
+        instr.setAssembly(newAssembly.replaceAll("mov", "mov dword"));
+    }
 
-	private String getConstant(Instr instruction) {
-		String instr = instruction.getAssembly();
-		int index = instr.indexOf(",");
-		return instr.substring(index + 1, instr.length());
-	}
+    private String getConstant(Instr instruction) {
+        String instr = instruction.getAssembly();
+        int index = instr.indexOf(",");
+        return instr.substring(index + 1, instr.length());
+    }
 }
--- lab3/ReachingDefinition.java
+++ lab3/ReachingDefinition.java
@@ -13,29 +13,28 @@ import graph.Node;
 
 public class ReachingDefinition {
 
-	private AssemFlowGraph cfg;
+    private AssemFlowGraph cfg;
 
-	private Map<Node, Set<Node>> in;
-	private Map<Node, Set<Node>> out;
-	private Map<Node, Set<Node>> gen;
-	private Map<Node, Set<Node>> kill;
+    private Map<Node, Set<Node>> in;
+    private Map<Node, Set<Node>> out;
+    private Map<Node, Set<Node>> gen;
+    private Map<Node, Set<Node>> kill;
 
-	/* defs(t) é o conjunto de nós que definem o temporário t */
-	private Map<Temp, Set<Node>> defs;
+    /* defs(t) é o conjunto de nós que definem o temporário t */
+    private Map<Temp, Set<Node>> defs;
 
-	public Set<Node> getIn(Node n){
-		return in.get(n);
-	}
+    public Set<Node> getIn(Node n){
+        return in.get(n);
+    }
 
-	public Set<Node> getOut(Node n){
-		return out.get(n);
-	}
+    public Set<Node> getOut(Node n){
+        return out.get(n);
+    }
 
-	public Set<Node> getKill(Node n){
-		return kill.get(n);
-	}
-	
-	public ReachingDefinition(List<Instr> l, AssemFlowGraph cfg) {
-	}
-
+    public Set<Node> getKill(Node n){
+        return kill.get(n);
+    }
+    
+    public ReachingDefinition(List<Instr> l, AssemFlowGraph cfg) {
+    }
 }
