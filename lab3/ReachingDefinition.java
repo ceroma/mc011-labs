f1844853993c3e182a4543e9a452123bd24e0a32
Carlos Eduardo Rosa Machado <carlos.rmachado@ymail.com>
2012-05-21 22:44:13 -0300

    Computing defs(t).

--- lab3/ReachingDefinition.java
+++ lab3/ReachingDefinition.java
@@ -20,7 +20,7 @@ public class ReachingDefinition {
     private Map<Node, Set<Node>> gen;
     private Map<Node, Set<Node>> kill;
 
-    /* defs(t) é o conjunto de nós que definem o temporário t */
+    // Set of all nodes that define the temporary t:
     private Map<Temp, Set<Node>> defs;
 
     public Set<Node> getIn(Node n){
@@ -36,5 +36,23 @@ public class ReachingDefinition {
     }
     
     public ReachingDefinition(List<Instr> l, AssemFlowGraph cfg) {
+        // Initializes D(t):
+        defs = new HashMap<Temp, Set<Node>>();
+        for (Node n : cfg.nodes()) {
+            // Consider only unambiguous definitions:
+            if (!cfg.getInstr(n).isMoveBetweenTemps()) {
+                continue;
+            }
+            
+            // Add current definition node to D(t):
+            for (Temp t : cfg.getDefined(n)) {
+                Set<Node> Dt = defs.get(t);
+                if (Dt == null) {
+                    Dt = new HashSet<Node>();
+                }
+                Dt.add(n);
+                defs.put(t, Dt);
+            }
+        }
     }
 }
